c387c731f660cad850073a68d2c4b5f7688e1be4
dork7 <weeday425@gmail.com>
2022-04-18 15:44:20 +0500

    reoute rfix

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,21 +1,18 @@
 import { Button } from "@chakra-ui/button";
 import { Center, HStack } from "@chakra-ui/layout";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = ({ pages }) => {
   return (
     <Center>
       <HStack p={4}>
         {pages.map((page, idx) => (
-          <Button
-            key={`${page}-${idx}`}
-            as="a"
-            href={page.href}
-            variant="solid"
-            colorScheme="teal"
-          >
-            {page.label}
-          </Button>
+          <Link key={`${page}-${idx}`} to={page.href}>
+            <Button key={`${page}-${idx}`} variant="solid" colorScheme="teal">
+              {page.label}
+            </Button>{" "}
+          </Link>
         ))}
       </HStack>
     </Center>
